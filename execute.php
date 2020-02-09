2d28e35a008cb2e86a737b4e77b085af28efae4b
giugiulietta <biabias@yahoo.com>
2020-02-09 16:02:58 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,41 +1,47 @@
 <?php
 
-$chatId = '---YOUR USER ID---';
+// recupero il contenuto inviato da Telegram
+$content = file_get_contents("php://input");
 
-$request = [
-    'chat_id' => $chatId,
-    'text' => 'Fai tap su condividi numero telefonico',
-    'reply_markup' => [
-        'keyboard' => [
-            [
-                [
-                    'text' => 'Condividi numero telefonico',
-                    'request_contact' => true
-                ]
-            ]
-        ],
-        'one_time_keyboard' => true,
-        'resize_keyboard' => true
-    ]
-];
+// converto il contenuto da JSON ad array PHP
+$update = json_decode($content, true);
 
-apiRequest('---INSERT API TOKEN HERE---', $request);
-
-function apiRequest(string $apiToken, array $request): array
+// se la richiesta è null interrompo lo script
+if(!$update)
 {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, sprintf('https://api.telegram.org/bot%s/sendMessage', $apiToken));
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-type: application/json']);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $content = curl_exec($ch);
-    curl_close($ch);
-
-    if(!$content) {
-
-        return [];
-    }
-
-    return json_decode($content, true);
+  exit;
 }
+
+// assegno alle seguenti variabili il contenuto ricevuto da Telegram
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
+$text = trim($text);
+
+// converto tutti i caratteri alfanumerici del messaggio in minuscolo
+$text = strtolower($text);
+
+// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
+// imposto l'header della risposta
+header("Content-Type: application/json");
+
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
+$parameters["method"] = "sendMessage";
+
+// imposto la keyboard
+$parameters["reply_markup"] = '{ "keyboard": [["uno"], ["due"], ["tre"], ["quattro"]], "one_time_keyboard": false}';
+
+// converto e stampo l'array JSON sulla response
+echo json_encode($parameters);
